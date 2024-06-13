762db0c539e34592f3fcf8dba149ef94464940e9
Francish <gautamfrancish@gmail.com>
2024-06-13 10:52:56 +0545

    submission

---
+++ src/main.ts
@@ -0,0 +1,192 @@
+import './style.css';
+
+// Game Board Settings
+let canvas;
+let canvasWidth = 360;
+let canvasHeight = 576;
+let ctx;
+
+// Doodler Settings
+let doodlerSize = { width: 46, height: 46 };
+let doodlerPos = { x: canvasWidth / 2 - doodlerSize.width / 2, y: canvasHeight * 7 / 8 - doodlerSize.height };
+let doodlerRightImage;
+let doodlerLeftImage;
+
+let doodler = {
+    img: null,
+    x: doodlerPos.x,
+    y: doodlerPos.y,
+    width: doodlerSize.width,
+    height: doodlerSize.height
+};
+
+// Physics Settings
+let velX = 0;
+let velY = 0; 
+let startVelY = -8; 
+let gravity = 0.4;
+
+// Platform Settings
+let platforms = [];
+let platformSize = { width: 60, height: 18 };
+let platformImage;
+
+//Score parameters
+let currentScore = 0;
+let topScore = 0;
+let isGameOver = false;
+
+window.onload = function() {
+    canvas = document.getElementById("board");
+    canvas.height = canvasHeight;
+    canvas.width = canvasWidth;
+    ctx = canvas.getContext("2d");
+
+    // Load images
+    doodlerRightImage = new Image();
+    doodlerRightImage.src = "/doodler-right.png";
+    doodler.img = doodlerRightImage;
+    doodlerRightImage.onload = function() {
+        ctx.drawImage(doodler.img, doodler.x, doodler.y, doodler.width, doodler.height);
+}
+
+    doodlerLeftImage = new Image();
+    doodlerLeftImage.src = "/doodler-left.png";
+
+    platformImage = new Image();
+    platformImage.src = "/platform.png";
+
+    velY = startVelY;
+    createPlatforms();
+    requestAnimationFrame(gameLoop);
+    document.addEventListener("keydown", controlDoodler);
+}
+
+function gameLoop() {
+    requestAnimationFrame(gameLoop);
+    if (isGameOver) return;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Move doodler
+    doodler.x += velX;
+    if (doodler.x > canvasWidth) {
+        doodler.x = 0;
+    } else if (doodler.x + doodler.width < 0) {
+        doodler.x = canvasWidth;
+    }
+
+    velY += gravity;
+    doodler.y += velY;
+    if (doodler.y > canvas.height) {
+        isGameOver = true;
+    }
+    ctx.drawImage(doodler.img, doodler.x, doodler.y, doodler.width, doodler.height);
+
+    // Move platforms
+    platforms.forEach(platform => {
+        if (velY < 0 && doodler.y < canvasHeight * 3 / 4) {
+            platform.y -= startVelY;
+        }
+        if (checkCollision(doodler, platform) && velY >= 0) {
+            velY = startVelY;
+        }
+        ctx.drawImage(platform.img, platform.x, platform.y, platform.width, platform.height);
+    });
+
+    // Remove off-screen platforms and add new ones
+    platforms = platforms.filter(platform => platform.y < canvasHeight);
+    while (platforms.length < 7) {
+        addNewPlatform();
+    }
+
+    // Update score
+    updateScore();
+    ctx.fillStyle = "black";
+    ctx.font = "16px sans-serif";
+    ctx.fillText(currentScore, 5, 20);
+
+    if (isGameOver) {
+        ctx.fillText("Game Over: Press 'Space' to Restart", canvasWidth / 7, canvasHeight * 7 / 8);
+    }
+}
+
+function controlDoodler(e) {
+    if (e.code === "ArrowRight" || e.code === "KeyD") {
+        velX = 4;
+        doodler.img = doodlerRightImage;
+    } else if (e.code === "ArrowLeft" || e.code === "KeyA") {
+        velX = -4;
+        doodler.img = doodlerLeftImage;
+    } else if (e.code === "Space" && isGameOver) {
+        resetGame();
+    }
+}
+
+function createPlatforms() {
+    platforms = [];
+    let initialPlatform = {
+        img: platformImage,
+        x: canvasWidth / 2,
+        y: canvasHeight - 50,
+        width: platformSize.width,
+        height: platformSize.height
+    };
+    platforms.push(initialPlatform);
+
+    for (let i = 0; i < 6; i++) {
+        let randomX = Math.random() * (canvasWidth * 3 / 4);
+        let platform = {
+            img: platformImage,
+            x: randomX,
+            y: canvasHeight - 75 * i - 150,
+            width: platformSize.width,
+            height: platformSize.height
+        };
+        platforms.push(platform);
+    }
+}
+
+function addNewPlatform() {
+    let randomX = Math.random() * (canvasWidth * 3 / 4);
+    let platform = {
+        img: platformImage,
+        x: randomX,
+        y: -platformSize.height,
+        width: platformSize.width,
+        height: platformSize.height
+    };
+    platforms.push(platform);
+}
+
+function checkCollision(a, b) {
+    return a.x < b.x + b.width &&
+           a.x + a.width > b.x &&
+           a.y < b.y + b.height &&
+           a.y + a.height > b.y;
+}
+
+function updateScore() {
+    if (velY < 0) {
+        topScore += Math.floor(Math.random() * 50);
+        currentScore = Math.max(currentScore, topScore);
+    } else if (velY >= 0) {
+        topScore -= Math.floor(Math.random() * 50);
+    }
+}
+
+function resetGame() {
+    doodler = {
+        img: doodlerRightImage,
+        x: doodlerPos.x,
+        y: doodlerPos.y,
+        width: doodlerSize.width,
+        height: doodlerSize.height
+    };
+    velX = 0;
+    velY = startVelY;
+    currentScore = 0;
+    topScore = 0;
+    isGameOver = false;
+    createPlatforms();
+}
